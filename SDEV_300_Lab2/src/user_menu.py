363a6ab1b9fd7ee7c6f371738d56001a5b3ae0ae
Keith Lee <keithmlee19@gmail.com>
2023-05-26 17:58:18 -0400

    Fixed pylint messages:
    Trailing whitespaces
    Lines too long
    Snake case
    Redid password generator branches

--- SDEV_300_Lab2/src/user_menu.py
+++ SDEV_300_Lab2/src/user_menu.py
@@ -5,7 +5,7 @@ import secrets
 from datetime import date
 import math
 
-def generatePassword():
+def gen_password():
     '''function to generate a password based on user specifications'''
     print("Passwords will minimally include lowercase letters.")
     alphabet = string.ascii_lowercase
@@ -17,43 +17,30 @@ def generatePassword():
         except ValueError:
             pass
         print("A secure password is >= 12 characters, please try again\n")
-    while True: # prompt user if they want uppercase letters
+    print("Please choose from the following additional options:")
+    print("1. Uppercase letters")
+    print("2. Digits")
+    print("3. Symbols")
+    print("4. Exit")
+    while True: # prompt user for their password specifications
         try:
-            pw_up = input("Do you want uppercase letters? y/n\n")
-            if pw_up.lower() == "y":
+            pw_choice = int(input("Choose an option\n"))
+            if (pw_choice == 1):
                 alphabet += string.ascii_uppercase
-                break
-            elif pw_up.lower() == "n":
-                break
-        except ValueError:
-            pass
-        print("Invalid input, please enter y or n\n")
-    while True: # prompt user if they want digits
-        try:
-            pw_num = input("Do you want digits? y/n\n")
-            if pw_num.lower() == "y":
+            elif (pw_choice == 2):
                 alphabet += string.digits
-                break
-            elif pw_num.lower() == "n":
-                break
-        except ValueError:
-            pass
-        print("Invalid input, please enter y or n\n")
-    while True: # prompt user if they want special characters
-        try:
-            pw_spec = input("Do you want special characters? y/n\n")
-            if pw_spec.lower() == "y":
+            elif (pw_choice == 3):
                 alphabet += string.punctuation
+            elif (pw_choice == 4):
                 break
-            elif pw_spec.lower() == "n":
-                break
+            else:
+                print("Invalid input, please enter a number 1-4\n")
         except ValueError:
             pass
-        print("Invalid input, please enter y or n\n")
-    password = ''.join(secrets.choice(alphabet) for i in range(pw_len))        
+    password = ''.join(secrets.choice(alphabet) for i in range(pw_len))
     print("The password you generated is: " + password + "\n\n")
 
-def calculatePercentage():
+def calc_percentage():
     '''Calculates a percentage and prints it with user-specified number of decimal places'''
     numerator = int(input("Please enter the numerator\n"))
     while True:
@@ -66,23 +53,23 @@ def calculatePercentage():
         print("Denominator cannot be 0, please try again\n")
     while True:
         try:
-            precision = int(input("Please enter the number of decimal places you want for formatting\n"))
-            if precision >= 0:
+            prec = int(input("Please enter the number of decimal places you want\n"))
+            if prec >= 0:
                 break
         except ValueError:
             pass
         print("Precision must be >= 0 decimal places, please try again\n")
     pct = numerator/denominator * 100
-    print("The percentage you calculated is: " + f"{pct:.{precision}f}" + " percent\n\n")
+    print("The percentage you calculated is: " + f"{pct:.{prec}f}" + " percent\n\n")
 
-def numDays():
+def num_days():
     '''Calculates days from today until July 4, 2025'''
-    targetDate = date(2025,7,4)
+    target_date = date(2025,7,4)
     today = date.today()
-    timeToTarget = abs(targetDate - today)
-    print("There are " + str(timeToTarget.days) + " days until July 4, 2025.\n\n")
+    time_to_target = abs(target_date - today)
+    print("There are " + str(time_to_target.days) + " days until July 4, 2025.\n\n")
 
-def lawOfCosines():
+def law_of_cosines():
     '''Calculates side c of a triangle based on sides a and b and angle C'''
     while True:
         try:
@@ -109,11 +96,12 @@ def lawOfCosines():
             pass
         print("Angle must be greater than 0 degrees, please try again\n")
     angle_c_rad = math.radians(angle_c)
-    side_c_squared = (side_a * side_a) + (side_b * side_b) - (2 * side_a * side_b * math.cos(angle_c_rad))
+    side_c_squared = (side_a * side_a) + (side_b * side_b) - (2 *
+                                                              side_a * side_b * math.cos(angle_c_rad))
     side_c = math.sqrt(side_c_squared)
     print("The length of side c is: " + f"{side_c:.{2}f}\n\n")
 
-def volRightCircCylinder():
+def vol_right_circ_cyl():
     '''Calculates volume of a right circular cylinder based on user input for radius and height'''
     while True:
         try:
@@ -131,10 +119,10 @@ def volRightCircCylinder():
         except ValueError:
             pass
         print("Height must be greater than 0, please try again\n")
-    volCylinder = math.pi * rad * rad * height
-    print("The volume of the cylinder is: " + f"{volCylinder:.{2}f}\n\n")
+    vol_cylinder = math.pi * rad * rad * height
+    print("The volume of the cylinder is: " + f"{vol_cylinder:.{2}f}\n\n")
     
-def handleMenu():
+def handle_menu():
     '''function to handle user menu input choices'''
     while True:
         try:
@@ -148,15 +136,15 @@ def handleMenu():
             print("f. Exit program")
             user_input = input("Please enter a choice to run\n")
             if user_input.lower() == "a":
-                generatePassword()
+                gen_password()
             elif user_input.lower() == "b":
-                calculatePercentage()
+                calc_percentage()
             elif user_input.lower() == "c":
-                numDays()
+                num_days()
             elif user_input.lower() == "d":
-                lawOfCosines()
+                law_of_cosines()
             elif user_input.lower() == "e":
-                volRightCircCylinder()
+                vol_right_circ_cyl()
             elif user_input.lower() == "f":
                 print("You've indicated that you want to exit. Thanks and goodbye\n")
                 sys.exit()
@@ -165,4 +153,4 @@ def handleMenu():
         except ValueError:
             pass
 
-handleMenu()
+handle_menu()
